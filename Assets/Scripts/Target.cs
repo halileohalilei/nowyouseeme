db0558ed6e4df0eaa222812f89b8e8a6b11ddb6c
Halil Ibrahim Kayim <kayimhalil@hotmail.com>
2015-07-05 17:51:14 +0300

    the real initial commit

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Target : MonoBehaviour
+    {
+        
+        private float _destroyTime;
+        [SerializeField] private float _lifeSpan;
+        void Start()
+        {
+            _destroyTime = Time.time + _lifeSpan;
+        }
+
+        void Update()
+        {
+            if (Time.time >= _destroyTime)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/TargetFactory.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TargetFactory : MonoBehaviour
+    {
+        [SerializeField] private float _spawnRate;
+        private float _timeCounter;
+
+        public GameObject Target;
+
+        void Awake()
+        {
+            _timeCounter = 0f;
+        }
+
+        void Update()
+        {
+            _timeCounter += Time.deltaTime;
+            if (_timeCounter/_spawnRate >= 1)
+            {
+                _timeCounter = _timeCounter % _spawnRate;
+                GameObject newTarget = Instantiate(Target, Vector3.forward, Quaternion.identity) as GameObject;
+                if (newTarget != null) newTarget.transform.parent = transform;
+            }
+        }
+    }
+}
