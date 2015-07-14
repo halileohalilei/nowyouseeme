9a91671f3018f289623038158182a2d5c34a7326
asmccormick <asmccormick@gmail.com>
2015-07-14 14:31:08 +0300

    more decoration to workshop scene

--- Assets/Scripts/ElfScript.js
+++ Assets/Scripts/ElfScript.js
@@ -2,6 +2,12 @@
 var anim : Animator;
 
 function Start () {
+	/*
+	// this won't actually start him moving... and i don't know why...
+	anim = GetComponent.<Animator>();
+	anim.Play("elf arm work");
+	*/
+	
 	anim = GetComponent.<Animator>();
 	Debug.Log("anim = " + anim);
 	//anim.SetFloat("Speed", 0.5);
