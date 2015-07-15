0a30ee859b626c055c8e4e0db424ed6c5c3f70f2
asmccormick <asmccormick@gmail.com>
2015-07-15 11:21:56 +0300

    music and a bouncing gift

---
+++ Assets/Scripts/GiftScript.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+var rb : Rigidbody;
+var force : float = 10;
+
+function Start () {
+	rb = GetComponent.<Rigidbody>();
+	transform.rotation = Random.rotation;
+}
+
+function Update () {
+	rb.AddRelativeForce(transform.forward * force);
+}
+
+function OnCollisionEnter(collision: Collision) {
+	Debug.Log("gift collides");
+	transform.rotation = Random.rotation;
+}
