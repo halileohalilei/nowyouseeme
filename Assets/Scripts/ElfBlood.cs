55a5b94423b949eab2f87141cfc2d2794779a58b
asmccormick <asmccormick@gmail.com>
2015-07-19 15:24:17 +0300

    elf blood prefab

--- Assets/Scripts/ElfBlood.cs
+++ Assets/Scripts/ElfBlood.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class ElfBlood : MonoBehaviour {
 
 	public GameObject[] candyArray;
-	int numCandy = 10;
+	int numCandy = 20;
 
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < numCandy; i++) {
 			int rand = Random.Range(0,candyArray.Length);
-			Vector3 loc = Random.insideUnitSphere * 10;
+			Vector3 loc = Random.insideUnitSphere * 1;
 			Quaternion rot = Random.rotation;
-			Instantiate(candyArray[rand], loc, rot);
+			GameObject instance = Instantiate(candyArray[rand], loc, rot) as GameObject;
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+			rb.AddExplosionForce(10, transform.position, 5, 3.0F);
 		}
 	
 	}
