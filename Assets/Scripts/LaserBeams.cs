c1e7ea1a0ee069831a2227191ab3569960e0c620
asmccormick <asmccormick@gmail.com>
2015-07-20 16:07:46 +0300

    laser beams target elves
    - deleted some unused materials
    - added laser beams to Jesus
    - beams target elves

---
+++ Assets/Scripts/LaserBeams.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBeams : MonoBehaviour {
+
+	public Transform[] targets;
+	public Transform currentTarget;
+	public Transform nextTarget;
+	public Quaternion dir1;
+	public Quaternion dir2;
+	public float startTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		currentTarget = targets[Random.Range(0,targets.Length)];
+		nextTarget = targets[Random.Range(0,targets.Length)];
+		dir1 = Quaternion.LookRotation(currentTarget.transform.position - transform.position);
+		dir2 = Quaternion.LookRotation(nextTarget.transform.position - transform.position);
+		transform.LookAt(currentTarget);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.rotation == dir2) {
+			startTime = Time.time;
+			dir1 = dir2;
+			nextTarget = targets[Random.Range(0,targets.Length)];
+			dir2 = Quaternion.LookRotation(nextTarget.transform.position - transform.position);
+		} else {
+		transform.rotation = Quaternion.Slerp(dir1, dir2, (Time.time - startTime));
+		}
+	}
+}
