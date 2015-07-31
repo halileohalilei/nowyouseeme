d919a4b316999a0c2dd2d3f3e3e87832df9c836b
SamiHamid <samihamid@hotmail.co.uk>
2015-07-31 19:01:55 +0300

    UI Phase I
    - First half of the UI has been built. Second half will happen tomorrow.

---
+++ Assets/GUI/GUIController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIController : MonoBehaviour {
+
+	private Animator anim;
+	public GameObject CoverHinge;
+
+	void Start () 
+	{
+		anim = CoverHinge.GetComponent<Animator> ();
+	}
+
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.O)) 
+		{
+			anim.SetTrigger("FrontOpen");
+		}
+
+		if (Input.GetKeyDown (KeyCode.C)) 
+		{
+			anim.SetTrigger("FrontClose");
+		}
+	}
+}
