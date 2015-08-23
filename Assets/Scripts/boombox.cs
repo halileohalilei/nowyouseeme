ff45bfa91b3de6356ccee0e28005322bd3319e29
asmccormick <asmccormick@gmail.com>
2015-08-23 17:41:34 +0300

    rubber boombox

--- Assets/Scripts/boombox.cs
+++ Assets/Scripts/boombox.cs
@@ -2,9 +2,10 @@
 
 namespace Assets.Scripts
 {
-    public class Boombox : Target {
+    public class boombox : Target {
 
         private Rigidbody _rigidbody;
+		private Vector3 _explosionPos;
         [SerializeField] private float _force;
 
         [SerializeField] private Jesus _jesus;
@@ -13,17 +14,20 @@ namespace Assets.Scripts
         {
 //            _jesus = GameObject.Find("Jesus").GetComponent<Jesus>();
             _rigidbody = GetComponent<Rigidbody>();
-            transform.rotation = Random.rotation;
+            //transform.rotation = Random.rotation;
+			_force = 10;
         }
 	
         // Update is called once per frame
         void Update () {
-            _rigidbody.AddRelativeForce(transform.forward * _force);
+            //_rigidbody.AddRelativeForce(transform.forward * _force);
         }
 
         void OnCollisionStay(Collision collision)
         {
-            transform.rotation = Random.rotation;
+            //transform.rotation = Random.rotation;
+			_explosionPos = transform.position + Random.insideUnitSphere * 0.5f;
+			_rigidbody.AddExplosionForce(1000.0f, _explosionPos, 5.0f, 0.0f);
         }
 
         public override string GetTargetType()
