cf082e4b61eb78d8fe3e4a95b2f3ded317d25bb2
SamiHamid <samihamid@hotmail.co.uk>
2015-09-28 22:39:19 +0300

    Splash Screen
    Splash Screen

---
+++ Assets/SplashScreen/AudioScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioScript : MonoBehaviour {
+
+	
+	void Start () 
+	{
+		Invoke ("AudioPlay", 4.5f);
+		Invoke ("LoadNextScene", 17.5f);
+	}
+	
+	void AudioPlay()
+	{
+		GetComponent<AudioSource>().Play ();
+	}
+	
+	void LoadNextScene()
+	{
+		Application.LoadLevel("_Scene01");
+		Debug.Log("LOADING NEXT SCENE");
+	}
+}
