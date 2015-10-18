2c8961b41176830aec6cd86682b72ba1921cb9fd
SamiHamid <samihamid@hotmail.co.uk>
2015-10-18 10:06:57 +0300

    123 Go Implemented

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -4,7 +4,14 @@ namespace Assets.Scripts
 {
     public class GameStarter : MonoBehaviour
     {
-
+		public GameObject Number1;
+		public GameObject Number2;
+		public GameObject Number3;
+		public GameObject GoObject;
+		
+		public AudioClip Bell1;
+		public AudioClip Bell2;
+		
         public float GameStartDelay;
 
         void OnTriggerEnter(Collider other)
@@ -13,12 +20,45 @@ namespace Assets.Scripts
                 other.transform.name.Equals("OVR"))
             {
                 Invoke("StartGame", GameStartDelay);
+                Invoke("No1", 1f);
+                Invoke("No2", 2f);
+                Invoke("No3", 3f);
+                Invoke("Go", 4f);
             }
         }
 
         private void StartGame()
         {
+        	GoObject.SetActive(false);
             GameData.GetCurrentGameData().StartGame();
+			
+        }
+        
+        private void No1()
+        {
+        	Number1.SetActive(true);
+			AudioSource.PlayClipAtPoint(Bell1,transform.position, 1.0f);
+        }
+        
+        private void No2()
+        {
+        	Number1.SetActive(false);
+        	Number2.SetActive(true);
+			AudioSource.PlayClipAtPoint(Bell1,transform.position, 1.0f);
+        }
+        
+        private void No3()
+        {
+        	Number2.SetActive(false);
+        	Number3.SetActive(true);
+			AudioSource.PlayClipAtPoint(Bell1,transform.position, 1.0f);
+        }
+        
+        private void Go()
+        {
+        	Number3.SetActive(false);
+        	GoObject.SetActive(true);
+			AudioSource.PlayClipAtPoint(Bell2,transform.position, 1.0f);
         }
 
     }
